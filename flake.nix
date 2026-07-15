c5ec2e899fcf4a8a63361d8db843d869341aa45b
LarsZauberer <wasser.ian@gmail.com>
2026-07-15 20:26:31 +0200

    GitOps: Add Blog autodeploy

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    kubenix.url = "github:hall/kubenix";
+    clusterLib.url = "github:LarsZauberer/nix-clusterlib";
   };
 
   outputs =
@@ -11,13 +13,13 @@
       self,
       nixpkgs,
       flake-utils,
+      kubenix,
+      clusterLib,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        lib = nixpkgs.lib;
-
         blogPackage = pkgs.buildNpmPackage {
           pname = "wasserxr-blog";
           version = "0.0.1";
@@ -54,36 +56,56 @@
         '';
       in
       {
-        packages = {
-          default = self.packages.${system}.docker;
-          docker = pkgs.dockerTools.buildLayeredImage {
-            name = "wasserxr-blog";
-            tag = "latest";
-            contents = [
-              pkgs.nginx
-              pkgs.fakeNss
-            ];
-            extraCommands = ''
-              mkdir -p var/log/nginx var/cache/nginx tmp srv/http
-              cp -r ${blogPackage}/. srv/http/
-            '';
-            config = {
-              Cmd = [
-                "${pkgs.nginx}/bin/nginx"
-                "-c"
-                "${nginxConf}"
-                "-g"
-                "daemon off;"
-              ];
-              ExposedPorts."80/tcp" = { };
+        packages =
+          let
+            kubernetes = (import ./kubernetes.nix) {
+              inherit pkgs;
+              lib = pkgs.lib;
+              inputs = {
+                inherit
+                  self
+                  nixpkgs
+                  flake-utils
+                  kubenix
+                  clusterLib
+                  ;
+              };
             };
+          in
+          {
+            default = self.packages.${system}.docker;
+            docker = pkgs.dockerTools.streamLayeredImage {
+              name = "wasserxr-blog";
+              contents = [
+                pkgs.nginx
+                pkgs.fakeNss
+              ];
+              extraCommands = ''
+                mkdir -p var/log/nginx var/cache/nginx tmp srv/http
+                cp -r ${blogPackage}/. srv/http/
+              '';
+              config = {
+                Cmd = [
+                  "${pkgs.nginx}/bin/nginx"
+                  "-c"
+                  "${nginxConf}"
+                  "-g"
+                  "daemon off;"
+                ];
+                ExposedPorts."80/tcp" = { };
+              };
+            };
+            kubernetes = kubernetes.config.kubernetes.result;
+            kubernetes-docker = kubernetes.config.docker.copyScript;
           };
-        };
 
         devShells.default = pkgs.mkShell {
           name = "devShell";
 
-          buildInputs = [ pkgs.nodejs ];
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.prefetch-npm-deps
+          ];
 
           shellHook = "";
         };
